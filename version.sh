70958575d4d353390a2b82ad40240d19a3a3b206
Robert Nelson <robertcnelson@gmail.com>
2020-01-20 17:40:07 -0600

    kernel bump: v5.4.13
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -34,10 +34,10 @@ toolchain="gcc_arm_aarch64_gnu_8"
 
 #Kernel
 KERNEL_REL=5.4
-KERNEL_TAG=${KERNEL_REL}.12
+KERNEL_TAG=${KERNEL_REL}.13
 kernel_rt=".10-rt5"
 #Kernel Build
-BUILD=${build_prefix}0
+BUILD=${build_prefix}1
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
